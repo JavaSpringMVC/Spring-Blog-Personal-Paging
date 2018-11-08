faab67c56811a66278c30535d13c90edfdc87629
nguyenhai9112 <nguyenhai9112@gmail.com>
2018-11-08 12:10:27 +0700

    list category edit + create + delete

--- src/main/java/com/hainguyen/blog/controller/CategoryController.java
+++ src/main/java/com/hainguyen/blog/controller/CategoryController.java
@@ -4,7 +4,11 @@ import com.hainguyen.blog.model.Category;
 import com.hainguyen.blog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,11 +16,53 @@ public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
-    @GetMapping("/category-list")
-    public ModelAndView listCategory(){
+    @GetMapping("/list-category")
+    public ModelAndView listCategory() {
         Iterable<Category> categories = categoryService.findAll();
         ModelAndView modelAndView = new ModelAndView("/category/list");
         modelAndView.addObject("categories", categories);
+        return modelAndView;
+    }
+
+    @GetMapping("/create-category")
+    public ModelAndView createCategoryForm() {
+        ModelAndView modelAndView = new ModelAndView("/category/create");
+        modelAndView.addObject("category", new Category());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-category")
+    public String saveCreateCategory(@ModelAttribute("category") Category category, Model model) {
+        categoryService.save(category);
+        model.addAttribute("message", "Create Category Successful");
+        return "redirect:/list-category";
+    }
+
+    @GetMapping("/delete-category/{id}")
+    public String deleteCategory(@PathVariable("id") int id, Model model) {
+        categoryService.remove(id);
+        model.addAttribute("message", "Delete Category Successful");
+        return "redirect:/list-category";
+    }
+
+    @GetMapping("/edit-category/{id}")
+    public ModelAndView editCategoryForm(@PathVariable("id") int id) {
+        Category category = categoryService.findById(id);
+        ModelAndView modelAndView;
+        if (category != null) {
+            modelAndView = new ModelAndView("/category/edit");
+            modelAndView.addObject("category", category);
+        }else {
+            modelAndView = new ModelAndView("/error-404");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/edit-category")
+    public ModelAndView updateCategory(@ModelAttribute("category") Category category) {
+        categoryService.save(category);
+        ModelAndView modelAndView = new ModelAndView("/category/edit");
+        modelAndView.addObject("message", "Edit category successful");
         return modelAndView;
     }
 }
